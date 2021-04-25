539b90b3b7aff3e5d941cdbec50241e59adeecb0
fangpf <452639095@qq.com>
2021-04-25 18:28:38 +0800

    add files

---
+++ resnet/cmake-build-debug/CMakeFiles/resnet.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "F:/AI/tensorRT-project/resnet/resnet50.cpp" "F:/AI/tensorRT-project/resnet/cmake-build-debug/CMakeFiles/resnet.dir/resnet50.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.2/include"
+  "C:/Program"
+  "../Files/NVIDIA"
+  "../GPU"
+  "../Computing"
+  "../Toolkit/CUDA/v10.2/include"
+  "D:/TensorRT-7.0.0.11/include"
+  "D:/TensorRT-7.0.0.11/samples/common/windows"
+  "D:/TensorRT-7.0.0.11/samples/common"
+  "D:/TensorRT-7.0.0.11/bin"
+  "../Toolkit/CUDA/v10.2/lib/x64"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ resnet/cmake-build-debug/CMakeFiles/resnet.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/resnet.dir/resnet50.cpp.obj"
+  "libresnet.dll.a"
+  "resnet.exe"
+  "resnet.exe.manifest"
+  "resnet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/resnet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
