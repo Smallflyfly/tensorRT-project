ebb6a089e22c94d2cf0b334fb291ea496f5c0fce
smallflyfly <452639095@qq.com>
2021-07-15 19:57:20 +0800

    code structure change

--- retinaface-onnx2trt/retinaFace.h
+++ retinaface-onnx2trt/retinaFace.h
@@ -7,7 +7,16 @@
 
 #include <iostream>
 #include <vector>
+#include "NvInfer.h"
+#include "logging.h"
+#include <fstream>
+#include <algorithm>
+#include "opencv2/opencv.hpp"
+#include "cuda_runtime.h"
 
+
+
+using namespace nvinfer1;
 using namespace std;
 
 #define CHECK(status) \
@@ -18,7 +27,18 @@ using namespace std;
         } \
     } while(0)
 
+static Logger gLogger;
+static const char *TRT_FILE = "retinaFace_1output.trt";
 
+static const int BATCH_SIZE = 1;
+static const int INPUT_W = 640;
+static const int INPUT_H = 640;
+static const char *INPUT_NAME = "input";
+static const char *OUTPUT_NAME = "output";
+static const int64_t OUTPUT_SIZE = 16800 * 16;
+static const int64_t OUTPUT_CHANNELS = 16800;
+static const float OBJ_THRESHOLD = 0.6;
+static const float NMS_THRESHOLD = 0.4;
 
 class RetinaFace {
 public:
@@ -72,5 +92,22 @@ public:
     };
 };
 
+ICudaEngine* readEngine();
+
+float iouCalculate(const RetinaFace::Detection &det1, const RetinaFace::Detection &det2);
+
+void nms(vector<RetinaFace::Detection> &detections);
+
+float *readImageData(cv::Mat &im, float &scale, int &pw, int &ph);
+
+vector<vector<float>> getAnchors();
+
+void decode(vector<vector<float>> &bboxes, const vector<vector<float>> &anchors);
+
+vector<RetinaFace::Detection> postProcess(float *output, const vector<vector<float>> &anchors);
+
+vector<RetinaFace::Detection> doInference(IExecutionContext &context, float *input, const vector<vector<float>> &anchors);
+
+void showResult(const cv::Mat &im, const vector<RetinaFace::Detection> &detections, float scale, int pw, int ph);
 
 #endif //RETINAFACE_ONNX2TRT_RETINAFACE_H
